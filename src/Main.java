9a7b6a6e0a9beaf2c967b2a9bf2d1f672dd6cd6c
ulyana <bsergeybabak2000@gmail.com>
2023-03-15 12:03:23 +0300

    третий коммит

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+
     }
 
     public static void task1() {
@@ -25,6 +28,25 @@ public class Main {
                 System.out.print(i + " ");
             }
         }
-
+    public static void task3(){
+        int population = 12_000_000;
+        int fertility = 17;
+        int mortality = 8;
+        for (int i = 1; i <= 10; i ++){
+            population = population+ population*17/1000 - mortality*8/1000;
+            System.out.println("Год " + i + " Численность населения составляет "
+                    + population);
+        }
+    }
+    public static void task4(){
+        int sum = 15_000;
+        int many = 0;
+        int i = 0;
+        for ( i = 1; i < 12_000_000 ; i +=  6){
+            sum = sum + sum * 7 / 100;
+            sum = many + i / 1050;
+            System.out.println("Месяц " + sum + " сумма " + i);
+        }
+    }
     }
 
